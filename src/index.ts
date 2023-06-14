7013488614dc8bc76f819962ef08305d24070f27
JD-Rosales <rosales.jake1999@gmail.com>
2023-06-14 20:03:24 +0800

    Add netlify cors origin

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,27 @@ const PORT: number = parseInt(process.env.PORT as string) || 5000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.use(cors({ origin: ['friendly-croissant-e5dcc5.netlify.app'] }));
+// app.use(
+//   cors({
+//     origin: ['friendly-croissant-e5dcc5.netlify.app'],
+//     credentials: true,
+//   })
+// );
+
+const whitelist = ['friendly-croissant-e5dcc5.netlify.app'];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin as string) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // api routes
